6231f09f12d96e07a2179a6758094daac6af548c
saude27 <akandesaudat2711@gmail.com>
2025-02-19 13:54:38 +0000

    created vpc files

--- variables.tf
+++ variables.tf
@@ -13,3 +13,45 @@ variable "environment" {
  description = "environment"
  type        = string
 }
+
+# vpc variables
+variable "vpc_cidr" {
+ description = "vpc cidr block"
+ type        = string
+}
+
+# public subnet az1
+variable "public_subnet_az1_cidr" {
+ description = "public subnet az1 cidr block"
+ type        = string
+}
+
+# public subnet az2
+variable "public_subnet_az2_cidr" {
+ description = "public subnet az2 cidr block"
+ type        = string
+}
+
+# private app subnet az1
+variable "private_app_subnet_az1_cidr" {
+ description = "private app subnet az1 cidr block"
+ type        = string
+}
+
+# private app subnet az2
+variable "private_app_subnet_az2_cidr" {
+ description = "private app subnet az2 cidr block"
+ type        = string
+}
+
+# private data subnet az1
+variable "private_data_subnet_az1_cidr" {
+ description = "private data subnet az1 cidr block"
+ type        = string
+}
+
+# private data subnet az2
+variable "private_data_subnet_az2_cidr" {
+ description = "private data subnet az2 cidr block"
+ type        = string
+}
