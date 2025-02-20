f25f06ffabd515a86ed0b48c386238a92fb76e2c
saude27 <akandesaudat2711@gmail.com>
2025-02-20 16:54:43 +0000

    created asg files

--- variables.tf
+++ variables.tf
@@ -105,3 +105,14 @@ variable "env_file_name" {
  description = "env file name"
  type        = string
 }
+
+# ecs variables
+variable "architecture" {
+ description = "ecs cpu architecture"
+ type        = string
+}
+
+variable "container_image" {
+ description = "container image uri"
+ type        = string
+ }
